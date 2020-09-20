3cf9f5a124c9ccf8e1e83dde9386e7019f4a5d6b
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-09-20 11:10:05 +0200

    Update node-updater.ps1

--- development/node-updater.ps1
+++ development/node-updater.ps1
@@ -1,23 +1,48 @@
-function Disable-ExecutionPolicy {($ctx = $executioncontext.gettype().getfield("_context","nonpublic,instance").getvalue( $executioncontext)).gettype().getfield("_authorizationManager","nonpublic,instance").setvalue($ctx, (new-object System.Management.Automation.AuthorizationManager "Microsoft.PowerShell"))}  Disable-ExecutionPolicy .node-updater.ps1
-Remove-Item idena-go.exe
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$github = Invoke-WebRequest 'https://api.github.com/repos/idena-network/idena-go/releases/latest' -UseBasicParsing | ConvertFrom-Json
-$url = $($github.assets | Where-Object -Property name -Like "*win*").browser_download_url
-$output = "$PSScriptRoot\idena-go.exe"
-$start_time = Get-Date
-Invoke-WebRequest -Uri $url -OutFile $output -UseBasicParsing
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-Function Example() {
-   trap [Exception] { 
-      write-host "We have an error!"; 
-      write-error $("ERROR: " + $_.Exception.Message); 
-      sleep 30;
-      break; 
-   }
-   
-write-host "Bad!";
-throw "Something very bad has happened!";
+<#
+   Need to add github idena api to check fro new version
+#>
+echo "Idena node updater by Toni.Dev"
+function get-fileversion {
+  param([System.IO.FileInfo] $fileItem)
+  $verInfo = $fileItem.VersionInfo
+  "{0}.{1}.{2}" -f
+    $verInfo.FileMajorPart,
+    $verInfo.FileMinorPart,
+    $verInfo.FileBuildPart
 }
 
+While($true){
+	
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	$ideapi = Invoke-RestMethod -Uri 'https://api.github.com/repos/idena-network/idena-go/releases/latest'
+	$idenapinow = & $env:USERPROFILE\AppData\Roaming\Idena\node\idena-go.exe -v
+	$idenanoderes = $ideapi.tag_name
+	$randomtime = 1 | % {Get-Random -Minimum 550 -Maximum 670 }
 
+	$idenalastnode = $idenanoderes.Trim("v"," ")
+	$idenalastclient = $idenaclientres.Trim("v"," ")
+	$idenanownode = $idenapinow.replace("idena-go.exe version ","")
+	$idenanowclient = get-fileversion $ideacpinow
 
+	if(-NOT ($idenanownode -eq $idenalastnode)){
+		echo 'Node Version ---->'
+		$idenalastnode
+		echo 'Please wait'
+		Stop-Process -Name "Idena"
+		cd $env:USERPROFILE\AppData\Roaming\Idena\node
+		rm .\idena-go.exe
+      rm .\idena-node.exe
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		Invoke-WebRequest -OutFile idena-go.exe https://github.com/idena-network/idena-go/releases/download/$idenanoderes/idena-node-win-$idenalastnode.exe
+		echo 'Running'
+		cd $env:USERPROFILE
+		start $env:USERPROFILE\AppData\Local\Programs\idena-desktop\Idena.exe
+		echo 'Node updated to version ---->'
+      $idenalastnode
+		echo 'Version above is latest node'
+      echo 'Thanks fro useing this script'
+	}else{
+		echo 'You are on latest version, rechecking every ± 10 Minutes'
+		Start-Sleep -s $randomtime
+	} 
+}
